647f6be71b0d0a5e479e093b53e92d11c6ef97c0
Lughi <lucianviziteu@gmail.com>
2018-02-13 14:48:21 +0200

    Footer scroll

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function log(x) {
-    console.log(x);
+  console.log(x);
 }
 
 var myMenu = document.getElementById("menu");
@@ -9,22 +9,31 @@ var mySecondPage = document.getElementById("secondPage");
 myMenu.addEventListener("click", change);
 
 function change() {
-    myFirstPage.classList.toggle("hidden");
-    mySecondPage.classList.toggle("hidden");
-    myMenu.classList.toggle("open");
+  myFirstPage.classList.toggle("hidden");
+  mySecondPage.classList.toggle("hidden");
+  myMenu.classList.toggle("open");
 }
-var nextJokeBtn = document.getElementById('jokeBtn')
-var loadingLogo = document.getElementById('loadingAnim')
-var jokeLine = document.getElementById('jokeText')
+var nextJokeBtn = document.getElementById("jokeBtn");
+var loadingLogo = document.getElementById("loadingAnim");
+var jokeLine = document.getElementById("jokeText");
 
-
-nextJokeBtn.addEventListener('click', onClick)
+nextJokeBtn.addEventListener("click", onClick);
 
 function onClick() {
-    loadingLogo.classList.toggle('hidden')
-    jokeLine.classList.toggle('hidden')
+  loadingLogo.classList.toggle("hidden");
+  jokeLine.classList.toggle("hidden");
 }
 
+var footer = document.getElementById("scrollEvent");
 
+footer.addEventListener("scroll", onScroll);
 
+function onScroll() {
+  if (footer.clientWidth + footer.scrollLeft >= footer.scrollWidth - footer.scrollWidth / 10) {
+    log(true);
+  } else log(false);
+}
 
+footer.scrollWidth;
+footer.scrollLeft;
+footer.clientWidth;
