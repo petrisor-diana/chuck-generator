536a9a2ef6cb514e51836bfdda01479f035f4d8e
Alex Lexy <alex@ACA0C9A8.ipt.aol.com>
2018-02-15 16:44:04 +0200

    proiect semi-final

--- main.js
+++ main.js
@@ -22,45 +22,63 @@ nextJokeBtn.addEventListener("click", onClick);
 function onClick() {
   jokeLine.classList.toggle("hidden");
   loadingLogo.classList.toggle("hidden");
-  
+
   var myRequest = new XMLHttpRequest;
 
   myRequest.addEventListener("load", addJoke)
-
-  
   myRequest.open("GET", "https://api.chucknorris.io/jokes/random");
   myRequest.send();
-  
+
 }
 
 function addJoke(e) {
   loadingLogo.classList.toggle("hidden");
   jokeLine.classList.toggle("hidden");
- 
+
   var myRequestAsText = e.srcElement.response;
   var myResponseAsAJSON = JSON.parse(myRequestAsText);
-  jokeLine.innerHTML= myResponseAsAJSON.value;
-
-  
-    
+  jokeLine.innerHTML = myResponseAsAJSON.value;
 }
-
 
 var footer = document.getElementById("scrollEvent");
 
 footer.addEventListener("scroll", onScroll);
 
-
 var timeoutId
+var nr=2;
+var footerImg=document.getElementById("imageGallery");
 function onScroll() {
   clearTimeout(timeoutId)
 
   if (footer.clientWidth + footer.scrollLeft >= footer.scrollWidth * 0.9) {
 
-    timeoutId = setTimeout(function () { log(true) }, 500);
-
-
+    timeoutId = setTimeout(function () {
+     
+      getPhotos(nr);
+      nr++;
+    }, 500);
   } else log(false);
 }
 
+var footerSpinner = document.getElementById("footerLoading");
 
+function getPhotos(nr) {
+  var myRequest = new XMLHttpRequest;
+  myRequest.addEventListener("load", function onLoad(e) {
+    footerSpinner.classList.add("hidden");
+    var myResponseAsText = e.srcElement.response;
+    var myResponseAsAJSON = JSON.parse(myResponseAsText);
+    addPhotos(myResponseAsAJSON)
+  });
+  myRequest.open("GET", "https://api.unsplash.com/photos/?page=" + nr + "&client_id=7551d068496476ab2e350913e3753a914f3ac6b83c12784e8cfc9c2a518cb1f8");
+  myRequest.send();
+  footerSpinner.classList.remove("hidden");
+};
+
+function addPhotos(list) {
+  for (var i = 0; i < list.length; i++) {
+    footerImg.innerHTML += "<img src=" + list[i].urls.regular + "/>";
+  }
+};
+
+getPhotos(1)
